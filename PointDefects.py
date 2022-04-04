d33a9a4ad0ec9ab085f26b10943a47e1ef5f4b14
mfox <mfox@fedora.io>
2022-04-04 11:53:29 -0400

    setup and discretize

--- PointDefects.py
+++ PointDefects.py
@@ -9,3 +9,34 @@ Written for the final project of MATH 578 at UTK - Spring 2022
 Purpose: Simulate the diffusion of point defects (vacancies and interstitials) in a material subject to irradiation damage.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+radFlux = 10**6     #particles/(cm^2*s)
+
+#define geometry - rectangular slab
+xmin = 0        #meters
+xmax = 1    #meters
+ymin = 0        #meters
+ymax = 5        #meters
+numdX = 5
+numdY = 25
+
+#spatial discretization/mesh
+grid = np.ones((numdY, numdX, 2)) #3rd dim represents concentrations of vacancies, interstitials (essentially 2 2D matrices)
+
+print(grid)
+
+
+#time discretization
+t_start = 0
+t_end = 10      #seconds
+numdT = 101    
+T, step = np.linspace(t_start, t_end, numdT, retstep=True)
+
+#BCs
+grid[0, :, :]= 0
+grid[:, 0, :] = 0 #zero defects on boundary
+
+#explicit forward euler
