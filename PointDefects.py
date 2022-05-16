1a8960aae4fc33dfd26b7f170b28d3cd514ac412
mfox <mfox@fedora.io>
2022-05-16 16:57:08 -0400

    mostly working implmeentation for vacancies, major refactoring of code

--- PointDefects.py
+++ PointDefects.py
@@ -22,8 +22,20 @@ from matplotlib.colors import Normalize as nm
 
 
 def main():
-    #sim parameters
+    ####inputs
+    #plot parameters
     plot_freq = 5  #wait this many time iterations before plotting
+    fig_size = (8,4) #size of saved plots; (x,y) in inches
+    fig_dpi = 300 #pixels per inch (affects plot quality and file size)
+    
+    #sim time
+    t_start = 0
+    t_end = 100  #seconds
+    numTnodes = 1001    #includes t=0, t = t_end;   number of time steps = numTnodes - 1
+    
+    #geometry size - square
+    side_length = 1 #cm
+    thickness = 0.1 #cm
     
     #set material properties
     flux = 1e8    #particles/cm^2*s
@@ -38,42 +50,38 @@ def main():
     E_disp_threshold = 40    #eV, energy required to produce a vacancy-interstitial pair.
     E_incident = 1e6    #eV (assumes monoenergetic incident radiation)
     temperature = 800   #K
+    ####end inputs
     
-    
+    ####setup
     #calculate macro material parameters
-    atomic_density = density * 6.022e23 / mass_num    #atoms/cm^3
+    atomic_density = density * 6.022e23 / mass_num   #atoms/cm^3
     macro_disp_cross_section = atomic_density * displacement_cross_section #cm^-1 ; probability of interaction per unit length traveled
-    sinkStrength_i = 1e-4
-    sinkStrength_v = 1e-4
+    sinkStrength_i = 0
+    sinkStrength_v = 0
     D_i = compute_diff(lattice_parameter, adjacent_lattice_sites, mass_num, E_i_jump_threshold, temperature)
     D_v = compute_diff(lattice_parameter, adjacent_lattice_sites, mass_num, E_v_jump_threshold, temperature)
     K_IV = recomb_num * (D_i + D_v) / (lattice_parameter)**2
     
-    #define geometry - square
-    s = 1 #cm (side length of square domain)
-    thickness = 0.1 #cm
-    
-    numXnodes = 5 #note: this includes zero and final time points
+    #define geometry
+    numXnodes = 11 #note: this includes zero and final spatial point
     numYnodes = numXnodes #force same spatial step sizes for simplicity
-    xmin = 0        #cm
-    xmax = s        #cm
-    ymin = 0        #cm
-    ymax = s        #cm
+    xmin = 0                #cm
+    xmax = side_length      #cm
+    ymin = 0                #cm
+    ymax = side_length       #cm
     num_atoms = (ymax-ymin)*(xmax-xmin)*thickness #doesn't do anything at the moment
     
     
     #time discretization
-    t_start = 0
-    t_end = 10  #seconds
-    numdT = 101
-    t, stepT = np.linspace(t_start, t_end, numdT, retstep=True)
+    t, stepT = np.linspace(t_start, t_end, numTnodes, retstep=True)
     
     #spatial discretization/mesh
-    ci = np.zeros((numXnodes, numYnodes, numdT), dtype=float) #initial values all zero
-    cv = np.zeros((numXnodes, numYnodes, numdT), dtype=float)
+    ci = np.zeros((numXnodes, numYnodes, numTnodes), dtype=float) #initial values all zero
+    cv = np.zeros((numXnodes, numYnodes, numTnodes), dtype=float)
     x, stepX = np.linspace(xmin, xmax, num=numXnodes, retstep=True)
     y, stepY = np.linspace(ymin, ymax, num=numYnodes, retstep=True)
-    
+    print(x)
+    print(y)
     
     #BCs
     ci[:,0,:] = 0   #dirichlet
@@ -85,8 +93,9 @@ def main():
     ci[numXnodes-1,:, :] = 0
     cv[numXnodes-1,:, :] = 0
     
+
     
-    #print parameters and run sim
+    #print important parameters
     print("Simulation Parameters *****************")
     print("Simulation Time: ", t_end)
     print("Time step: ", stepT)
@@ -101,71 +110,54 @@ def main():
     print("Radiation Flux: ", flux)
     print("****************************************\n")
     
-   ############################################################
-       
-    gen = atomic_density * flux_to_DPA(flux, displacement_cross_section, mass_num, E_incident, E_disp_threshold) #TODO second check
-    
-    alpha = [D_v * stepT / (2 * stepX**2), D_i * stepT / (2*stepX**2)]  #combine leading coefficient in CN method for simpler notation. (Note: alpha different for vacancy/interstitial)
+############################################################
+    #solver step   
     m = numXnodes-2
     k = numYnodes-2 #excludes BC terms
+    
+    alpha = [D_v * stepT / (2 * stepX**2), D_i * stepT / (2*stepX**2)]  #combine leading coefficient in CN method for simpler notation. (Note: alpha different for vacancy/interstitial)
     
     #build (m*k)x(m*k) vacancy matrix (store as sparse for memory savings)
     ones = np.ones(m*k) #vector filled with ones to build diagonals
     diag_entries = np.array([(1-4*alpha[0])*ones, alpha[0]*ones, alpha[0]*ones, alpha[0]*ones, alpha[0]*ones]) #need main diag, two adjacent diags, and two additional diags
+    offsets = np.array([0, -1, 1, k-1, -(k-1)])
+    A_v = sp.dia_array((diag_entries, offsets), shape=(m*k,m*k)) #construct array in sparse form
+    print(A_v.todense())
+    
+    #build interstitial matrix (maybe I should do this with toeplitz?)
+    diag_entries = np.array([(1-4*alpha[1])*ones, alpha[1]*ones, alpha[1]*ones, alpha[1]*ones, alpha[1]*ones]) #need main diag, two adjacent diags, and two additional off-band diags
     offsets = np.array([0, -1, 1, k, -k])
-    A_v = sp.dia_array((diag_entries, offsets), shape=(m,k)) #construct array in sparse form
+    A_i = sp.dia_array((diag_entries, offsets), shape=(m*k,m*k)) #construct array in sparse form
     
+    gen = atomic_density * flux_to_DPA(flux, displacement_cross_section, mass_num, E_incident, E_disp_threshold)   #TODO second check
     
-    #build interstitial matrix
-    diag_entries = np.array([(1-4*alpha[1])*ones, alpha[1]*ones, alpha[1]*ones, alpha[1]*ones, alpha[1]*ones]) #need main diag, two adjacent diags, and two additional diags
-    offsets = np.array([0, -1, 1, k, -k])
-    A_i = sp.dia_array((diag_entries, offsets), shape=(m,k)) #construct array in sparse form
+    #TODO starter step for AB2 multistep method
+    #F_v[0:2] = 
     
-    #start AB2 multistep method
-    #TODO
-    
-    for t_iter in range(1, numdT-1):
+    for t_iter in range(1, numTnodes-1):
         for x_iter in range(0, numXnodes-1):
             for y_iter in range(0, numYnodes-1):
                 
-                recomb_now = compute_recomb(ci, cv, K_IV, x_iter, y_iter, t_iter-1)
-                recomb_prev = compute_recomb(ci, cv, K_IV, x_iter, y_iter, t_iter-2)
-                
                 #TODO: code AB2 here
                 F_v = np.zeros(m*k)
-                F_v[x_iter+y_iter] = 3/2 * (gen - recomb_now - compute_sink(cv, sinkStrength_v, D_v, x_iter, y_iter, t_iter - 1)) + 1/2 * (gen - recomb_prev - compute_sink(cv, sinkStrength_v, D_v, x_iter, y_iter, t_iter - 2))
+                F_v[x_iter+y_iter] = 3/2 * dcdt(cv, ci, gen, D_v, K_IV, sinkStrength_v, x_iter, y_iter, t_iter-1) - 1/2 * dcdt(cv, ci, gen, D_v, K_IV, sinkStrength_v, x_iter, y_iter, t_iter-2)
                 
                 #construct b matrix
                 b_v = np.zeros(m*k)
                 b_v[x_iter+y_iter] = F_v[x_iter+y_iter] + (1-4*alpha[0]) * cv[x_iter, y_iter, t_iter-1] + alpha[0] * cv[x_iter+1, y_iter, t_iter-1] + alpha[0] * cv[x_iter-1, y_iter, t_iter-1] + alpha[0] * cv[x_iter, y_iter-1, t_iter-1] + alpha[0] * cv[x_iter, y_iter+1, t_iter-1]
                 
-        x = sp.linalg.spsolve(A_v, b_v) #matrix dimensions apparently incompatible
+        x_v, converged_code = sp.linalg.minres(A_v, b_v) #solve system with efficient iterative method
         
-        #TODO map x to cv
+        if (converged_code != 0):
+            raise RuntimeError('Failed to converge!')
         
-        print(x)
-            
-        if (t_iter % plot_freq == 0 or t_iter == numdT-2): #plot and save png
-            plt.figure(figsize = (8,4))
-            conc = np.transpose(ci[:,:,t_iter]) #transpose to make x/y axis plot correctlyi
-            time = "".join(["Time: ", str(r'{:.3f}'.format(t_iter*stepT)), " sec"]) #tuple -> string, keep following zeros after decimal point
-            plt.suptitle(time)
-            plt.subplot(1,2,1)
-            
-            plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud')
-            plt.title("Interstitial Conc. (cm^-3)")
-            plt.colorbar()
-            
-            plt.subplot(1,2,2)
-            conc = np.transpose(cv[:,:,t_iter]) #transpose to make x/y axis plot correctly
-            
-            plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud')
-            plt.title("Vacancy Conc. (cm^-3)")
-            plt.colorbar()
-            
-            filename = "".join(["PointDefects",str(t_iter),".png"]) #tuple -> string
-            plt.savefig(filename, dpi = 300)
-            plt.show()
+        #TODO map x to c row by row
+        for i in range(1, m):
+            cv[1:(m+1), i, t_iter] = x_v[m*(i-1):m*i]
+        
+        
+        if (t_iter % plot_freq == 0 or t_iter == numTnodes-1): #plot and save png
+            plot_and_save(ci, cv, t_iter, stepT, fig_size, fig_dpi)
             
     print("Done!")
 
@@ -187,7 +179,30 @@ def compute_sink(func, strength, D, x, y, t):
 def compute_recomb(c1, c2, KIV, x, y, t):
     return KIV * c1[x,y,t] * c2[x,y,t]
 
-#############
+def dcdt(c1, c2, gen, D, KIV, sinkstrength, x, y, t): #note: this function excludes diffusion
+    return gen - compute_recomb(c1, c2, KIV, x, y, t) - compute_sink(c1, sinkstrength, D, x, y, t)
+
+def plot_and_save(ci, cv, t, time_step, figsize, imgdpi):
+    plt.figure(figsize = (8,4))
+    conc = np.transpose(ci[:,:,t]) #transpose to make x/y axis plot correctly
+    time = "".join(["Time: ", str(r'{:.3f}'.format(t*time_step)), " sec"]) #tuple -> string, keep following zeros after decimal point
+    plt.suptitle(time)
+    plt.subplot(1,2,1)
+            
+    plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud')
+    plt.title("Interstitial Conc. (cm^-3)")
+    plt.colorbar()
+            
+    plt.subplot(1,2,2)
+    conc = np.transpose(cv[:,:,t]) #transpose to make x/y axis plot correctly
+            
+    plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud')
+    plt.title("Vacancy Conc. (cm^-3)")
+    plt.colorbar()
+            
+    filename = "".join(["PointDefects",str(t),".png"]) #tuple -> string
+    plt.savefig(filename, dpi=imgdpi)
+    plt.show()
 
 #run
 if __name__ == "__main__":
