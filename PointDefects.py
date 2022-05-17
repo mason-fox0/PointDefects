7a7ee443bb4b296376ba066802c7f99b1cad7d67
mfox <mfox@fedora.io>
2022-05-17 11:25:58 -0400

    non-working implementation CNAB for interstitials

--- PointDefects.py
+++ PointDefects.py
@@ -16,6 +16,8 @@ Numerical Method: Crank-Nicholson Adams Bashforth (2-step) IMEX Scheme
 import math
 import numpy as np
 import scipy as sp
+import scipy.sparse.linalg
+import scipy.linalg
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize as nm
 
@@ -23,7 +25,7 @@ from matplotlib.colors import Normalize as nm
 def main():
     ####inputs
     #plot parameters
-    plot_freq = 5  #wait this many time iterations before plotting
+    plot_freq = 1  #wait this many time iterations before plotting
     fig_size = (8,4) #size of saved plots; (x,y) in inches
     fig_dpi = 300 #pixels per inch (affects plot quality and file size)
     
@@ -31,13 +33,13 @@ def main():
     #sim time
     t_start = 0
     t_end = 10  #seconds
-    numTnodes = 1001    #includes t=0, t = t_end;   number of time steps = numTnodes - 1
+    numTnodes = 101    #includes t=0, t = t_end;   number of time steps = numTnodes - 1
     
     
     #geometry size - square
     side_length = 1 #cm
     thickness = 0.1 #cm
-    
+    numSpatialNodes = 51
     
     #set material properties
     flux = 1e8    #particles/cm^2*s
@@ -51,7 +53,7 @@ def main():
     E_i_jump_threshold = 1.5     #ev/atom, energy required for interstitial to diffuse #TODO: find a real value
     E_disp_threshold = 40    #eV, energy required to produce a vacancy-interstitial pair.
     E_incident = 1e6    #eV (assumes monoenergetic incident radiation)
-    temperature = 800   #K
+    temperature = 600   #K
     ####end inputs
     
     
@@ -69,7 +71,7 @@ def main():
     
     
     #define geometry
-    numXnodes = 11 #note: this includes zero and final spatial point
+    numXnodes = 51 #note: this includes zero and final spatial point
     numYnodes = numXnodes #force same spatial step sizes for simplicity
     xmin = 0                #cm
     xmax = side_length      #cm
@@ -83,7 +85,7 @@ def main():
     
     
     #spatial discretization/mesh
-    ci = np.ones((numXnodes, numYnodes, numTnodes), dtype=float) #initial values all zero
+    ci = np.zeros((numXnodes, numYnodes, numTnodes), dtype=float) #initial values all zero
     cv = np.zeros((numXnodes, numYnodes, numTnodes), dtype=float)
     x, stepX = np.linspace(xmin, xmax, num=numXnodes, retstep=True)
     y, stepY = np.linspace(ymin, ymax, num=numYnodes, retstep=True)
@@ -124,7 +126,7 @@ def main():
     alpha = [D_v * stepT / (2 * stepX**2), D_i * stepT / (2*stepX**2)]  #combine leading coefficient in CN method for simpler notation. (Note: alpha different for vacancy/interstitial)
     
     #build (m*k)x(m*k) vacancy matrix
-    firstrow = np.zeros(m*k) #characterize toeplitz matrix with first row, first column (these should be the same)
+    firstrow = np.zeros(m*k) #characterize toeplitz matrix (constant diag matrix) with first row, first column (these should be the same)
     firstrow[0] = (1-4*alpha[0])
     firstrow[1] = alpha[0]
     firstrow[m+1] = alpha[0]
@@ -137,33 +139,42 @@ def main():
     firstrow[m+1] = alpha[1]
     A_i = sp.linalg.toeplitz(firstrow) #build matrix
     
-    gen = atomic_density * flux_to_DPA(flux, displacement_cross_section, mass_num, E_incident, E_disp_threshold)   #TODO second check
+    gen = int(atomic_density * flux_to_DPA(flux, displacement_cross_section, mass_num, E_incident, E_disp_threshold))   #TODO second check
     
     #TODO starter step for AB2 multistep method
     for t_iter in range(2, numTnodes):
         F_v = np.zeros(m*k) #reset
         b_v = np.zeros(m*k)
         b_v[1] = stepT*gen #start with euler's method (note b_v[0] = zero, as well as other terms for j=1)
-        #TODO: tracking of j and nodes incompatible. if statement???
+        
+        F_i = np.zeros(m*k)
+        b_i = np.zeros(m*k)
+        b_i[1] = stepT*gen #start with euler's method (note b_v[0] = zero, as well as other terms for j=1)
+        
+        #TODO: tracking of j and nodes incompatible, fix
         j=2 #counter for terms in b matrix
         for x_iter in range(1, numXnodes-1):
             for y_iter in range(1, numYnodes-1):
                 
                 F_v[j] = 3/2 * dcdt(cv, ci, gen, D_v, K_IV, sinkStrength_v, x_iter, y_iter, t_iter-1) - 1/2 * dcdt(cv, ci, gen, D_v, K_IV, sinkStrength_v, x_iter, y_iter, t_iter-2)
-                
                 b_v[j] = stepT * F_v[j] + (1-4*alpha[0]) * cv[x_iter, y_iter, t_iter-1] + alpha[0] * cv[x_iter+1, y_iter, t_iter-1] + alpha[0] * cv[x_iter-1, y_iter, t_iter-1] + alpha[0] * cv[x_iter, y_iter-1, t_iter-1] + alpha[0] * cv[x_iter, y_iter+1, t_iter-1]
+                
+                F_i[j] = 3/2 * dcdt(ci, cv, gen, D_i, K_IV, sinkStrength_i, x_iter, y_iter, t_iter-1) - 1/2 * dcdt(ci, cv, gen, D_i, K_IV, sinkStrength_i, x_iter, y_iter, t_iter-2)
+                b_i[j] = stepT * F_v[j] + (1-4*alpha[1]) * ci[x_iter, y_iter, t_iter-1] + alpha[1] * ci[x_iter+1, y_iter, t_iter-1] + alpha[1] * ci[x_iter-1, y_iter, t_iter-1] + alpha[1] * ci[x_iter, y_iter-1, t_iter-1] + alpha[0] * ci[x_iter, y_iter+1, t_iter-1]
                 
                 j=j+1
                 
                 
-        x_v, converged_code = sp.sparse.linalg.gmres(A_v, b_v) #solve system with efficient iterative method
+        x_v, exitcode_v = sp.sparse.linalg.gmres(A_v, b_v) #solve system with efficient iterative method
+        x_i, exitcode_i = sp.sparse.linalg.gmres(A_i, b_i)
         
-        if (converged_code != 0):
+        if (exitcode_v or exitcode_i != 0):
             raise RuntimeError('Failed to converge!')
         
         #map x to c row by row
         for i in range(1, m):
             cv[1:(m+1), i, t_iter] = x_v[m*(i-1):m*i]
+            ci[1:(m+1), i, t_iter] = x_i[m*(i-1):m*i]
         
         
         if (t_iter % plot_freq == 0 or t_iter == numTnodes-1): #plot and save png
@@ -189,7 +200,7 @@ def compute_sink(func, strength, D, x, y, t):
     return strength * D * func[x, y, t]
 
 def compute_recomb(c1, c2, KIV, x, y, t):
-    return KIV * c1[x,y,t] * c2[x,y,t]
+    return KIV * int(c1[x,y,t]) * int(c2[x,y,t]) #take advantage of no max int size to prevent overflow
 
 def dcdt(c1, c2, gen, D, KIV, sinkstrength, x, y, t): #note: this function excludes diffusion
     return gen - compute_recomb(c1, c2, KIV, x, y, t) - compute_sink(c1, sinkstrength, D, x, y, t)
@@ -200,15 +211,17 @@ def plot_and_save(ci, cv, t, time_step, figsize, imgdpi):
     time = "".join(["Time: ", str(r'{:.3f}'.format(t*time_step)), " sec"]) #tuple -> string, keep following zeros after decimal point
     plt.suptitle(time)
     plt.subplot(1,2,1)
-            
-    plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud')
+    
+    plt.pcolormesh(conc,edgecolors='none', norm=nm())        #plot without shading
+    #plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud') #plot with shading
     plt.title("Interstitial Conc. (cm^-3)")
     plt.colorbar()
             
     plt.subplot(1,2,2)
     conc = np.transpose(cv[:,:,t]) #transpose to make x/y axis plot correctly
             
-    plt.pcolormesh(conc,edgecolors='none', norm=nm())
+    plt.pcolormesh(conc,edgecolors='none', norm=nm())   #plot without shading
+    #plt.pcolormesh(conc,edgecolors='none', norm=nm(), shading='gouraud') #plot with shading
     plt.title("Vacancy Conc. (cm^-3)")
     plt.colorbar()
             
